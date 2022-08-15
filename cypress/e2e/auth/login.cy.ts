b69503b75ff1122ddfe27e8a6edd5821ab926ba9
Ju-yeop <dhwnduq12@naver.com>
2022-08-15 17:01:23 +0900

    E2E Testing

---
+++ cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,29 @@
+describe("Log In", () => {
+  const user = cy;
+  it("should go to homepage", () => {
+    user.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+  it("can see email / password validation errors", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("bad@email");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("yeop@naver.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("err")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+  it("can fill out the form and login", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("owner@naver.com");
+    user.findByPlaceholderText(/password/i).type("123");
+    user
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click();
+    user.window().its("localStorage.nuber-token").should("be.a", "string");
+  });
+});
