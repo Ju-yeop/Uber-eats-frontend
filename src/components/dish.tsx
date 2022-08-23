c3d325133fc61fb4b647965b0ba880304badb858
Ju-yeop <dhwnduq12@naver.com>
2022-08-23 22:25:47 +0900

    Making an Order

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -1,19 +1,65 @@
 import React from "react";
+import { myRestaurant_myRestaurant_restaurant_menu_options } from "../__generated__/myRestaurant";
 
 interface IDishProps {
+  id?: number;
   description: string;
   name: string;
   price: number;
+  isCustomer?: boolean;
+  orderStarted?: boolean;
+  options?: myRestaurant_myRestaurant_restaurant_menu_options[] | null;
+  addItemToOrder?: (dishId: number) => void;
+  removeFromOrder?: (dishId: number) => void;
+  isSelected?: boolean;
+  children?: JSX.Element | JSX.Element[];
 }
 
-export const Dish: React.FC<IDishProps> = ({ description, name, price }) => {
+export const Dish: React.FC<IDishProps> = ({
+  id = 0,
+  description,
+  name,
+  price,
+  isCustomer = false,
+  orderStarted = false,
+  options,
+  addItemToOrder,
+  isSelected,
+  removeFromOrder,
+  children: dishOptions,
+}) => {
+  const onClick = () => {
+    if (orderStarted) {
+      if (!isSelected && addItemToOrder) {
+        return addItemToOrder(id);
+      }
+      if (isSelected && removeFromOrder) {
+        return removeFromOrder(id);
+      }
+    }
+  };
   return (
-    <div className=" px-8 py-4 border cursor-pointer hover:border-gray-800 transition-all ">
+    <div
+      className={` px-8 py-4 border cursor-pointer  transition-all ${
+        isSelected ? "border-gray-800" : " hover:border-gray-800"
+      }`}
+    >
       <div className="mb-5">
-        <h3 className="text-lg font-medium ">{name}</h3>
+        <h3 className="text-lg font-medium ">
+          {name}{" "}
+          {orderStarted && (
+            <button onClick={onClick}>{isSelected ? "Remove" : "Add"}</button>
+          )}
+        </h3>
         <h4 className="font-medium">{description}</h4>
       </div>
       <span>${price}</span>
+      {isCustomer && options && options?.length !== 0 && (
+        <div>
+          <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
+          {dishOptions}
+        </div>
+      )}
     </div>
   );
 };
